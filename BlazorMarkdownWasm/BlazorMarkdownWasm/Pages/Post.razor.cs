2df32976b56e2cd585b6f9265afd0c1dc3877888
Dennis Moon <dmoon.comfycoder@gmail.com>
2020-07-02 06:45:44 -0500

    Added a Blazor Standalone WASM markdown viewer example

---
+++ BlazorMarkdownWasm/BlazorMarkdownWasm/Pages/Post.razor.cs
@@ -0,0 +1,44 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorMarkdownWasm.Pages
+{
+    public partial class Post
+    {
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        [Inject]
+        IJSRuntime jsRuntime { get; set; }
+
+        public string RawHtml { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            var response = await Http.GetStringAsync("posts/markdown-monster/markdown-monster.md");
+
+            var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+
+            var result = Markdown.ToHtml(response, pipeline);
+
+            RawHtml = result;
+        }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                
+            }
+
+            await jsRuntime.InvokeVoidAsync("highlightInterop.highlightCode");
+        }
+    }
+}
---
+++ BlazorMarkdownWasm/BlazorMarkdownWasm/wwwroot/js/highlightInterop.js
@@ -0,0 +1,8 @@
+﻿var highlightInterop = highlightInterop || {};
+
+highlightInterop.highlightCode = function () {
+    var pres = document.querySelectorAll("pre>code");
+    for (var i = 0; i < pres.length; i++) {
+        hljs.highlightBlock(pres[i]);
+    }
+};
